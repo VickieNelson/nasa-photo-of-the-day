7a909c6e08a588f5752d37fba557a0ad10957e4d
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-13 15:49:05 -0500

    app.js mocked out

--- src/App.js
+++ src/App.js
@@ -2,17 +2,38 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
+import Post from "./posts.js";
+
 function App() {
+  const [nasaData, updateData] = useState({});
+
+  useEffect(() => {
+    axios
+
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+
+      .then((response) => {
+        // displaying data from api for inspection
+        console.log(res.data);
+
+        updateData(res.data);
+
+        console.log(`${nasaData} is the data`);
+      })
+
+      .catch((err) => {
+        console.log("Yoda says: return you must!");
+      });
+  });
+
   return (
     <div className='App'>
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun{" "}
-        <span role='img' aria-label='go!'>
-          🚀
-        </span>
-        !
-      </p>
+      <Post
+        title={nasaData.title}
+        date={nasaData.date}
+        url={nasaData.url}
+        summary={nasaData.explanation}
+      />
     </div>
   );
 }
